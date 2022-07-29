2351904795ee851be431376f65b9ab25368c3988
Quentin Stievenard <qstievenard@gmail.com>
2022-07-29 18:43:22 +0200

    fix home page + navigation

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,24 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <main>
+    <main className="home-container">
       <h2 className="pt-8 mx-4 text-5xl font-bold w-full leading-relaxed">
         Bienvenue sur
-        <span className="sportsee-red">Sportsee</span>
+        <span className="text-sportsee-red"> Sportsee</span>
       </h2>
       <div className="py-8 mx-4 w-2/3">
         <ul>
           <li>
-            <a href="/user/12">User 12</a>
+            <a className="underline text-neutral-400" href="/user/12">
+              User 12
+            </a>
           </li>
           <li>
-            <a href="/user/18">User 18</a>
+            <a className="underline text-neutral-400" href="/user/18">
+              User 18
+            </a>
           </li>
         </ul>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,9 @@ import logo from "../assets/images/logo.svg";
 export default function Header() {
   return (
     <div className="bg-black flex items-center justify-between pl-7 pr-19 shadow-md z-10 py-4.5">
-      <img src={logo} alt="logo SportSee" />
+      <a href="/">
+        <img src={logo} alt="logo SportSee" />
+      </a>
       <p className="text-white text-2xl cursor-pointer">Accueil</p>
       <p className="text-white text-2xl cursor-pointer">Profil</p>
       <p className="text-white text-2xl cursor-pointer">Réglage</p>
